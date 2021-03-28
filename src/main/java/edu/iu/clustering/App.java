c10b0ab0f85ffcc09ff2a255d5eb2781b6856bcb
Chathura Widanage <chathurawidanage@gmail.com>
2021-03-27 23:17:29 -0400

    initial shuffle implementation

--- src/main/java/edu/iu/clustering/App.java
+++ src/main/java/edu/iu/clustering/App.java
@@ -3,10 +3,33 @@ package edu.iu.clustering;
 import mpi.MPI;
 import mpi.MPIException;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 public class App {
+
+  private static Logger LOG = Logger.getLogger(App.class.getName());
+
   public static void main(String[] args) throws MPIException {
     MPI.Init(args);
     int rank = MPI.COMM_WORLD.getRank();
     int worldSize = MPI.COMM_WORLD.getSize();
+    LOG.info("Starting worker " + rank + " of " + worldSize);
+
+    Random random = new Random(rank);
+    int size = random.nextInt(100);
+
+    LOG.info("Size of data in " + rank + " : " + size);
+    int[] nodes = new int[size];
+    int[] clusters = new int[size];
+    for (int i = 0; i < size; i++) {
+      nodes[i] = Math.abs(random.nextInt());
+    }
+    NodePayload payload = new NodePayload(nodes, clusters);
+
+    Shuffle shuffle = new Shuffle();
+    shuffle.shuffle(payload);
+
+    MPI.Finalize();
   }
 }
--- src/main/java/edu/iu/clustering/Shuffle.java
+++ src/main/java/edu/iu/clustering/Shuffle.java
@@ -2,6 +2,7 @@ package edu.iu.clustering;
 
 import mpi.MPI;
 import mpi.MPIException;
+import mpi.Request;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
@@ -9,18 +10,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Shuffle {
 
-  private int getTag(int w1, int w2) {
-    ByteBuffer tagBuffer = ByteBuffer.allocate(4);
-    return -1;
+  private static Logger LOG = Logger.getLogger(Shuffle.class.getName());
+
+  /**
+   * This method assumes that the world size is less than 127 and fits into a short
+   *
+   * @param w1 worker id of the first worker
+   * @param w2 worker id of the second worker
+   * @return tag for the communication between w1 and w2
+   */
+  public int getTag(int w1, int w2) {
+    int tag = Math.min(w1, w2) | Math.max(w1, w2) << 16;
+    return tag;
   }
 
   public NodePayload shuffle(NodePayload localGraph) throws MPIException {
     int worldSize = MPI.COMM_WORLD.getSize();
     int thisWorker = MPI.COMM_WORLD.getRank();
 
+    // todo replace with primitive arrays if there is a significant impact on performance
     List<Integer>[] partitions = new List[worldSize];
     for (int i = 0; i < worldSize; i++) {
       partitions[i] = new ArrayList<>();
@@ -42,11 +54,44 @@ public class Shuffle {
       }
     }
 
+    ByteBuffer[] recvBuffers = new ByteBuffer[worldSize];
+
+    List<Request> pendingRequests = new ArrayList<>();
+
     // now communicate sizes
     for (int i = 0; i < worldSize; i++) {
       if (i != thisWorker) {
-//        MPI.COMM_WORLD.iSend(
-//            IntBuffer.allocate(1).put(partitions[i].size()), 1, MPI.INT, i, );
+        Request sendReq = MPI.COMM_WORLD.iSend(
+            ByteBuffer.allocateDirect(Integer.SIZE).putInt(partitions[i].size()), 1, MPI.INT, i, thisWorker);
+        recvBuffers[i] = ByteBuffer.allocateDirect(Integer.SIZE);
+        Request recvReq = MPI.COMM_WORLD.iRecv(recvBuffers[i], 1, MPI.INT, i, i);
+        pendingRequests.add(sendReq);
+        pendingRequests.add(recvReq);
+      }
+    }
+
+    // wait for communication
+    for (Request pendingRequest : pendingRequests) {
+      pendingRequest.waitFor();
+    }
+
+    // clear old requests
+    pendingRequests.clear();
+
+    // now we have the sizes
+    for (int i = 0; i < worldSize; i++) {
+      if (i != thisWorker) {
+        int size = recvBuffers[i].getInt();
+        LOG.info("Worker " + i + " will send " + size + " to worker " + thisWorker);
+        ByteBuffer sendingBuffer = ByteBuffer.allocateDirect(Integer.SIZE * partitions[i].size());
+        for (Integer integer : partitions[i]) {
+          sendingBuffer.putInt(integer);
+        }
+        Request sendReq = MPI.COMM_WORLD.iSend(sendingBuffer, partitions[i].size(), MPI.INT, i, thisWorker);
+        recvBuffers[i] = ByteBuffer.allocateDirect(Integer.SIZE * size);
+        Request recvReq = MPI.COMM_WORLD.iRecv(recvBuffers[i], size, MPI.INT, i, i);
+        pendingRequests.add(sendReq);
+        pendingRequests.add(recvReq);
       }
     }
 
