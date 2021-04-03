956ee8e8dee12a25c3f48df9f411f3e57e6f6101
Chathura Widanage <chathurawidanage@gmail.com>
2021-04-02 20:21:03 -0400

    fixing a bug in GraphBuilder

--- src/main/java/edu/iu/clustering/GraphBuilder.java
+++ src/main/java/edu/iu/clustering/GraphBuilder.java
@@ -2,7 +2,6 @@ package edu.iu.clustering;
 
 import com.opencsv.CSVReader;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,7 +12,7 @@ public class GraphBuilder {
 
     public static NodePayload buildGraph(String filePath) {
         CSVReader reader = null;
-        LinkedHashMap<Integer, ArrayList> edgeList = new LinkedHashMap<>();
+        LinkedHashMap<Integer, ArrayList<Integer>> edgeList = new LinkedHashMap<>();
         try {
             reader = new CSVReader(new FileReader(filePath));
             String[] nextLine; //read one line at a time
@@ -22,23 +21,20 @@ public class GraphBuilder {
                     String[] edge = token.split(" ");
                     int startNode = Integer.parseInt(edge[0].trim());
                     int endNode = Integer.parseInt(edge[1].trim());
-                    edgeList.computeIfAbsent(startNode, list -> new ArrayList()).add(endNode);
-                    edgeList.computeIfAbsent(endNode, list -> new ArrayList());
+                    edgeList.computeIfAbsent(startNode, list -> new ArrayList<>()).add(endNode);
+                    edgeList.computeIfAbsent(endNode, list -> new ArrayList<>()).add(startNode);
                 }
             }
             int[][] adjacencyMatrix = new int[edgeList.keySet().size()][edgeList.keySet().size()];
-            List<Integer> listKeys = new ArrayList<Integer>(edgeList.keySet());
-            int[] nodes = listKeys.stream().mapToInt(key -> (int) key).toArray();
+            List<Integer> listKeys = new ArrayList<>(edgeList.keySet());
+            int[] nodes = listKeys.stream().mapToInt(key -> key).toArray();
             listKeys.forEach(key -> {
-                ArrayList list = edgeList.get(key);
+                ArrayList<Integer> list = edgeList.get(key);
                 list.forEach(value -> {
                     adjacencyMatrix[listKeys.indexOf(key)][listKeys.indexOf(value)] = 1;
                 });
             });
             return new NodePayload(nodes, new int[nodes.length], adjacencyMatrix);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
