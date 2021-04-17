43f8418a382ba53ae2f5fb3bd2b1e7ea750c25b7
Chathura Widanage <chathurawidanage@gmail.com>
2021-04-17 15:41:31 -0400

    reverting large files

---
+++ src/main/resources/data/amazon/amazon.sh
@@ -0,0 +1,6 @@
+wget https://snap.stanford.edu/data/bigdata/communities/com-amazon.ungraph.txt.gz
+gzip -d com-amazon.ungraph.txt.gz
+tail -n +5 com-amazon.ungraph.txt > com-amazon.ungraph.txt.new
+rm com-amazon.ungraph.txt
+mv com-amazon.ungraph.txt.new com-amazon.ungraph.txt
+split -l231469 --numeric-suffixes=1 --suffix-length=1 --additional-suffix=".txt"  com-amazon.ungraph.txt "amazon-"
---
+++ src/main/resources/data/livejournal/livejournal.sh
@@ -0,0 +1,6 @@
+wget https://snap.stanford.edu/data/bigdata/communities/com-lj.ungraph.txt.gz
+gzip -d com-lj.ungraph.txt.gz
+tail -n +5 com-lj.ungraph.txt > com-lj.ungraph.txt.new
+rm com-lj.ungraph.txt
+mv com-lj.ungraph.txt.new com-lj.ungraph.txt
+split -l8670298 --numeric-suffixes=1 --suffix-length=1 --additional-suffix=".txt"  com-lj.ungraph.txt "li-"
