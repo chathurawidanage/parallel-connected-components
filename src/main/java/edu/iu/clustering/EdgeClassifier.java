f36a9c613b9411529718ef1cbd6c26491da4bc00
Isuru Ranawaka <irjanith@gmail.com>
2021-04-22 15:38:47 -0400

    naive classifier

--- src/main/java/edu/iu/clustering/EdgeClassifier.java
+++ src/main/java/edu/iu/clustering/EdgeClassifier.java
@@ -59,7 +59,7 @@ public class EdgeClassifier {
         }
     }
 
-    public static void partitionNaively(String[] args) throws IOException {
+    public static void partitionRoundRobin(String[] args) throws IOException {
         String sourceFile = args[0];
         int noOfPartitions = Integer.parseInt(args[1]);
         BufferedReader reader = null;
@@ -113,10 +113,79 @@ public class EdgeClassifier {
         }
     }
 
+    public static void partitionNaively(String[] args) throws IOException {
+        String sourceFile = args[0];
+        int noOfPartitions = Integer.parseInt(args[1]);
+        BufferedReader reader = null;
+        BufferedReader reader2 = null;
+        HashMap<String, BufferedWriter> writerHashMap = new HashMap<>();
+        HashMap<String, FileWriter> fileHashMap = new HashMap<>();
+        LinkedHashMap<Integer, Set<Integer>> edgeList = new LinkedHashMap<>();
+        try {
+            reader = new BufferedReader(new FileReader(sourceFile));
+            String next;
+            int totalCount = 0;
+            while ((next = reader.readLine()) != null && !next.equals("")) {
+                totalCount++;
+            }
+            int diff = totalCount % noOfPartitions;
+            int partitionSize = (totalCount - diff)/ noOfPartitions;
+
+            String nextLine; //read one line at a time
+            String dstFile = null;
+            int count = 1;
+            int multiple = 1;
+            reader2 = new BufferedReader(new FileReader(sourceFile));
+            while ((nextLine = reader2.readLine()) != null && !reader2.equals("")) {
+                if (count < multiple* partitionSize) {
+                    dstFile = sourceFile + "-" + multiple + ".txt";
+                } else {
+                    multiple++;
+                    dstFile = sourceFile + "-" + multiple + ".txt";
+                }
+                File file = new File(dstFile);
+
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+
+                FileWriter fw = fileHashMap.get(dstFile);
+                if (fw == null) {
+                    fw = new FileWriter(file, true);
+                    fileHashMap.put(dstFile, fw);
+                }
+
+                BufferedWriter bufferedWriter = writerHashMap.get(dstFile);
+                if (bufferedWriter == null) {
+                    bufferedWriter = new BufferedWriter(fw);
+                    writerHashMap.put(dstFile, bufferedWriter);
+                }
+                bufferedWriter.write(nextLine);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                count++;
+
+            }
+            System.out.println("Exist");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            writerHashMap.forEach((s, bw) -> {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length == 1) {
             EdgeClassifier.writeToFile(args[0]);
         } else if (args.length ==2) {
+//            EdgeClassifier.partitionRoundRobin(args);
             EdgeClassifier.partitionNaively(args);
         }
     }
